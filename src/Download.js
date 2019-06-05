da8700b903cfd929cebc91d1981d034c96eaa1c0
Nutcha Suwannayik <nutcha_nt@Nutchas-MacBook-Air.local>
2019-06-05 09:31:13 +0700

    first commit

---
+++ src/Download.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Download(){
+    return(
+        <h1>Download</h1>
+    );
+}
+
+export default Download;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from 'firebase';
+import Home from './Home';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const config = {
+    apiKey: "AIzaSyAkCp9gAP6dOHj_e36yf-6a4hBu0G_ulmk",
+    databaseURL: "https://i4cblockchain.firebaseio.com",
+  };
+
+firebase.initializeApp(config);
+
+
+ReactDOM.render(<Home/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
