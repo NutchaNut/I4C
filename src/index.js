4d47156c0d600b62889592cdb450c173689fe240
Nutcha Suwannayik <nutcha_nt@Nutchas-MacBook-Air.local>
2019-06-08 17:28:18 +0700

    edit index

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ const config = {
 firebase.initializeApp(config);
 
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+ReactDOM.render(<Home/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
